8b79801434bd543c4081e5fbf7b53c6172b36649
Ryan <ryanpinas77@gmail.com>
2025-04-09 20:54:02 +0200

    first commit

---
+++ library/library.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Création de la struct des livres
+type Book struct {
+	Title       string
+	Author      string
+	ID          int
+	IsAvailable bool
+}
+
+func main() {
+	// Création de plusieurs livres
+	book1 := Book{Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", ID: 1, IsAvailable: true}
+	book2 := Book{Title: "1984", Author: "George Orwell", ID: 2, IsAvailable: false}
+	book3 := Book{Title: "To Kill a Mockingbird", Author: "Harper Lee", ID: 3, IsAvailable: true}
+
+	// Création du slice de livres
+	library := []Book{book1, book2, book3}
+
+	// Affichage des livres
+	for _, book := range library {
+		fmt.Printf("📘 Titre: %s | Auteur: %s | ID: %d | Disponible: %t\n",
+			book.Title, book.Author, book.ID, book.IsAvailable)
+	}
+
+}
